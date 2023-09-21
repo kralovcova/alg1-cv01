223763f1052e391aa7e343ed7a1f58e7ebff0006
kralovcova <jirina.kralovcova@tul.cz>
2023-09-21 12:13:56 +0200

    Reseni zadanych uloh

---
+++ src/kralovcova/VypocetDle.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2023 Jirina Kralovcova
+ * This program is part of Java programing education. 
+ * You can use it as you need to learn Java. 
+ */
+
+package kralovcova;
+
+/**
+ *
+ * @author Jirina
+ */
+public class VypocetDle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("zadej vstupy");
+        System.out.println("provadim co je treba");
+        System.out.println("Vypis vysledku");
+    }
+
+}
